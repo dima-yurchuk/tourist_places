16260d634e9c1fa49d8a78ee19e66ffb8275b83c
dima-yurchuk <dimayurchuk20@gmail.com>
2022-04-01 16:45:44 +0300

    add profile updating

--- app/user/forms.py
+++ app/user/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, Length, Regexp, EqualTo, \
     ValidationError
+from flask_wtf.file import FileField, FileAllowed
 
 from app.user.models import User
 
@@ -21,6 +22,12 @@ def check_symbols(form, string):
     if string.data.strip().isalnum():
         raise ValidationError('Пароль повинен містити символ(не літеру і не '
                               'цифру)')
+
+
+def check_file_size(form, field):
+    if field.data:
+        if len(field.data.read()) > 3000000:
+            raise ValidationError('Ромір файлу не повинен перевищувати 3 MB')
 
 
 def check_spaces(form, string):
@@ -86,3 +93,51 @@ class RegistrationForm(FlaskForm):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError(
                 "Користувач з таким іменем уже зареєстрований!")
+
+
+class AccountUpdateForm(FlaskForm):
+    username = StringField(
+        "Ім'я користувача",
+        validators=[Length(min=3, max=30,
+                           message='Поле повинно бути довжиною '
+                                   'від 3 до 30 симолів!'),
+                    DataRequired(message='Заповніть це поле!'),
+                    Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                           "Ім'я повинно містити тільки англійські літери, "
+                           "цифри, крапку або нижнє підкреслення!")
+                    ]
+    )
+    picture = FileField('Фото профілю',
+                        validators=[
+                            FileAllowed(['jpg', 'png', 'jpeg'],
+                                        message='Файл повинен мати одне з '
+                                                'наступних розширень: '
+                                                '.jpg, .png, .jpeg'),
+                            check_file_size])
+    submit = SubmitField('Оновити')
+
+
+class PasswordUpdateForm(FlaskForm):
+    old_password = PasswordField(
+        'Старий пароль',
+        validators=[
+            DataRequired(message='Заповніть це поле!'),
+        ]
+    )
+    password = PasswordField(
+        'Пароль',
+        validators=[Length(min=8, max=30,
+                           message='Пароль повинен бути довжиною від 8 до 30 '
+                                   'симолів!'),
+                    DataRequired(message='Заповніть це поле!'),
+                    check_letters, check_digits,
+                    check_symbols, check_spaces],
+        render_kw={"placeholder": "повинен включати велику та малу літери,"
+                                  " цифру та спецсимвол"}
+    )
+    confirm_password = PasswordField(
+        'Підтвердження паролю',
+        validators=[DataRequired(message='Заповніть це поле!'),
+                    EqualTo('password', message='Паролі не збігаються!')]
+    )
+    submit = SubmitField('Змінити')
--- app/user/views.py
+++ app/user/views.py
@@ -1,12 +1,30 @@
 from flask import render_template, redirect, url_for, flash, request, \
     current_app, session, jsonify
 from flask_login import login_user, current_user, logout_user, login_required
+from wtforms import ValidationError
 
 from . import user_bp
-from app import db
-from .forms import LoginForm, RegistrationForm
+from app import db, bcrypt
+from .forms import LoginForm, RegistrationForm, AccountUpdateForm, PasswordUpdateForm
 from .models import User
 from ..tourist_places.models import Place, Type
+from PIL import Image
+import os, secrets
+
+
+def save_picture(form_picture):
+    rendom_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = rendom_hex + f_ext
+    picture_path = os.path.join(user_bp.root_path, '../static/pictures/profile_img',
+                                picture_fn)
+    # form_picture.save(picture_path)
+    # return  picture_fn
+    output_size = (100, 100)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
 
 
 @user_bp.route('/login', methods=['GET', 'POST'])
@@ -77,6 +95,57 @@ def account_list(action):
     place_id_list = []
     for place_type in place_types:
         place_id_list.append(place_type.place_id)
-    favourite_places = db.session.query(Place)\
+    favourite_places = db.session.query(Place) \
         .filter(Place.id.in_(place_id_list)).all()
     return render_template('account_list.html', places=favourite_places)
+
+
+@user_bp.route("/account/update/<action>", methods=['GET', 'POST'])
+@login_required
+def account_update(action):
+    form_account = AccountUpdateForm()
+    form_password = PasswordUpdateForm()
+    if request.method == 'GET':
+        form_account.username.data = current_user.username
+    if action == 'main':
+        if form_account.validate_on_submit():
+            if form_account.picture.data:
+                picture_file = save_picture(form_account.picture.data)
+                current_user.picture = picture_file
+            if current_user.username == form_account.username.data:
+                pass
+            elif User.query.filter_by(username=form_account.username.data).first():
+                flash('Користувач з таким іменем вже існує', 'danger')
+                return redirect(url_for('user_bp_in.account_update', action='main'))
+            current_user.username = form_account.username.data
+            try:
+                db.session.commit()
+                flash('Дані успішно оновлено', 'info')
+                return redirect(url_for('user_bp_in.account'))
+            except:
+                db.session.rollback()
+                flash('Помилка при оновленні даних', 'danger')
+                return redirect(url_for('user_bp_in.account_update', action='main'))
+    if action == 'password':
+        if form_password.validate_on_submit():
+            print('good')
+            if current_user.verify_password(form_password.old_password.data):
+                current_user.password = bcrypt.generate_password_hash(
+                    form_password.
+                    password.data). \
+                    decode('utf-8')
+                try:
+                    db.session.commit()
+                    flash('Пароль успішно змінено', 'info')
+                    return redirect(url_for('user_bp_in.account'))
+                except:
+                    db.session.rollback()
+                    flash('Помилка при оновленні даних', 'danger')
+                    return redirect(
+                        url_for('user_bp_in.account_update', action='main'))
+            else:
+                flash('Неправильний старий пароль', 'danger')
+                return redirect(url_for('user_bp_in.account_update',
+                                        action='password'))
+    return render_template('account_update.html', form_account=form_account,
+                           form_password=form_password)
